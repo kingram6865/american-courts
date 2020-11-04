5a2a1aed7042ef34cbfd6a92f198fcdf334c2f6c
Ken Ingram <ken.ingram@gmail.com>
2020-11-03 19:09:37 -0800

    Figuring out best wayt to retrieve links

--- process.js
+++ process.js
@@ -3,6 +3,8 @@ const path = require('path')
 const fs = require('fs')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom;
+const mysql = require('mysql')
+const datasource = require('./services/config')
 const files = []
 const metadata = {
   title: '',
@@ -11,85 +13,106 @@ const metadata = {
 }
 const results = []
 const url_prefix = "https://en.wikipedia.org"
+
 /**
- * 
  * If link is red:
  * https://en.wikipedia.org/w/index.php?title=Davison%27s_Lessee_v._Bloomer&action=edit&redlink=1
  * 
  * If link is blue
  * https://en.wikipedia.org/wiki/Respublica_v._De_Longchamps
- * 
- * 
- * 
  */
+
+
+
+function proccessHTML(fragment, source){
+  const dom = new JSDOM(fragment["*"])
+  const listitems = dom.window.document.getElementsByTagName('li')
+  
+  for (let x of listitems) {
+    const record = {
+      case_name: '',
+      case_links: [],
+      volume: source
+    }
+    let tester = x.getElementsByTagName('a')[0].getAttribute('href')
+    if (/_v\./.test(tester)) {
+      const anchors = x.getElementsByTagName('a').length
+      record.case_name = x.textContent
+      if (anchors > 1) {
+        for (let i=0; i <= anchors)
+        console.log(x.getElementsByTagName('a')[0].getAttribute('href'), x.getElementsByTagName('a')[1].getAttribute('href'))
+        // x.getElementsByTagName('a').map((item, index) => {
+        //   record.case_links.push(url_prefix + item[index].getAttribute('href'))
+        // })
+      } else {
+        record.case_links.push(url_prefix + x.getElementsByTagName('a')[0].getAttribute('href'))
+      }
+      insertData(record)
+      // if (anchors === 2){
+      //   record.case_name = x.textContent
+      //   record.case_links.push(url_prefix + x.getElementsByTagName('a')[0].getAttribute('href'))
+      //   record.case_links.push(url_prefix + x.getElementsByTagName('a')[1].getAttribute('href'))
+      //   // console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.getElementsByTagName('a')[1].getAttribute('href')} -- ${x.textContent}`)
+      // // console.log(JSON.stringify(record))
+      // } else if (anchors === 3) {
+      //   record.case_name = x.textContent
+      //   record.case_links.push(url_prefix + x.getElementsByTagName('a')[0].getAttribute('href'))
+      //   record.case_links.push(url_prefix + x.getElementsByTagName('a')[1].getAttribute('href'))
+      //   record.case_links.push(url_prefix + x.getElementsByTagName('a')[2].getAttribute('href'))
+      //   // console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.getElementsByTagName('a')[1].getAttribute('href')} -- ${x.getElementsByTagName('a')[2].getAttribute('href')} -- ${x.textContent}`)
+      // // console.log(JSON.stringify(record))
+      // } else {
+      //   record.case_name = x.textContent
+      //   record.case_links.push(url_prefix + x.getElementsByTagName('a')[0].getAttribute('href'))
+      //   // console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.textContent}`)
+      // // console.log(JSON.stringify(record))
+      // insertData(record)
+      // }
+
+      // console.log(JSON.stringify(record))
+      // results.push(record)
+    }
+    // console.log(JSON.stringify(record))
+    // console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.getElementsByTagName('a')[1].getAttribute('href')} -- ${x.textContent}`)
+    // console.log(x.getElementsByTagName('a').length)
+  }
+
+  // console.log(`Results list size: ${results.length}`)
+}
 
 function processData(input) {
   let rawdata = fs.readFileSync(input)
   let wiki = JSON.parse(rawdata)
-  /**
-   * title
-   * text
-   * externallinks
-   */
-  // console.log(wiki.parse['title'])
-  // console.log(wiki.parse['externallinks'])
+
   metadata.title = wiki.parse['title']
   metadata.externallinks = wiki.parse['externallinks']
 
-  console.log(metadata.title)
-  console.log(metadata.externallinks.length)
-
-  proccessHTML(wiki.parse['text'])
+  proccessHTML(wiki.parse['text'], wiki.parse['title'])
 }
 
-function proccessHTML(fragment){
-  const dom = new JSDOM(fragment["*"])
-  const doclist = dom.window.document.getElementsByTagName("a")
+function insertData(data) {
+  // const input = data.map
+  const conn = mysql.createConnection(datasource)
+  let sql = "INSERT INTO supreme_court (case_name, url, volume) VALUES ?,?";
+  // async conn.query(sql, )
+  console.log(data)
+}
 
-  const listitems = dom.window.document.getElementsByTagName('li')
-  
-  for (let x of listitems) {
-    const anchors = x.getElementsByTagName('a').length
-    if (anchors === 2){
-      console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.getElementsByTagName('a')[1].getAttribute('href')} -- ${x.textContent}`)
-    } else if (anchors === 3) {
-      console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.getElementsByTagName('a')[1].getAttribute('href')} -- ${x.getElementsByTagName('a')[2].getAttribute('href')} -- ${x.textContent}`)
+
+
+// =================================
+function execute() {
+  fs.readdirSync(directory).forEach(file => {
+    if (fs.lstatSync(path.resolve(directory, file)).isDirectory()) {
+      console.log(`Directory: ${file}`)
     } else {
-      console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.textContent}`)
+      // console.log(`File: ${file}`)
+      // processData(`${directory}/${file}`)
+      // console.log(`${directory}/${file}`)
+      files.push(`${directory}/${file}`)
     }
-
-
-    // console.log(`${x.getElementsByTagName('a')[0].getAttribute('href')} -- ${x.getElementsByTagName('a')[1].getAttribute('href')} -- ${x.textContent}`)
-    // console.log(x.getElementsByTagName('a').length)
-  }
-/*
-  for (let item of doclist) {
-    const response = {
-      url: '',
-      text: ''
-    }
-
-    response.text = item.textContent
-    response.url = item.href
-    results.push(response)
-    // console.log(`${item.textContent} __ ${item.innerHTML}`)
-  }
-*/
+  })
 }
-
-
-fs.readdirSync(directory).forEach(file => {
-  if (fs.lstatSync(path.resolve(directory, file)).isDirectory()) {
-    console.log(`Directory: ${file}`)
-  } else {
-    // console.log(`File: ${file}`)
-    // processData(`${directory}/${file}`)
-    // console.log(`${directory}/${file}`)
-    files.push(`${directory}/${file}`)
-  }
-})
-
 // console.log(files[0])
+execute()
 processData(files[0])
-
-// console.log(results[10])
